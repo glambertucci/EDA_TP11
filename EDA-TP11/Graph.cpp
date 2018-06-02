cc79ef65bdce59e458a23d55d7e7498265cb7fee
Francisco Martin Tolaba <ftolaba@itba.edu.ar>
2018-06-02 12:31:24 -0300

    pusk_back PUTO

--- EDA-TP11/Graph.cpp
+++ EDA-TP11/Graph.cpp
@@ -1,10 +1,23 @@
 #include "Graph.h"
+using namespace std;
 
-
-
-Graph::Graph(unsigned int nodes)
+Graph::Graph(unsigned int node)
 {
 	// Crea todos los nodos y hace el grafo
+	for (int i = 0; i < node; i++)
+		this->nodes.push_back(Node());
+
+	for (int i = 1; i < node-1; i++)
+	{
+		this->nodes[i].nextn = &(nodes[i + 1]);
+		this->nodes[i].prevn = &(nodes[i - 1]);
+	}
+
+	this->nodes[0].prevn = &(nodes[node -1]);
+	this->nodes[0].nextn = &(nodes[1]);
+	this->nodes[node - 1].prev = &(nodes[node - 2]);
+	this->nodes[node - 1].nextn = &(nodes[0]);
+
 }
 
 
